ce00935dddb3fc0dea896fe771d7bd5245bb6d75
Manikandan R <rmanikandan102000@gmail.com>
2023-04-11 14:25:01 +0530

    doctor service added

---
+++ Doctor_service/Controllers/DoctorAvailblityController.cs
@@ -0,0 +1,73 @@
+﻿using Doctor_Business_Logic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorAvailblityController : ControllerBase
+    {
+        private readonly ILogic _logic;
+        public DoctorAvailblityController(ILogic logic)
+        {
+            _logic = logic;
+        }
+
+        [HttpGet("particular/{email}")]
+        public ActionResult GetAva([FromRoute] string email)
+        {
+            var users = _logic.GetDoctrAv(email);
+            if (users != null)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return BadRequest(users);
+            }
+        }
+        [HttpPost("Add")]
+        public ActionResult Post([FromBody] doctor_availability user)
+        {
+            var users = _logic.ADD(user);
+            if (users != null)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return BadRequest(users);
+            }
+        }
+
+        [HttpPut("Update/{email}")]
+        public ActionResult Put([FromRoute] string email, [FromBody] doctor_availability u)
+        {
+            var users = _logic.UpdateDoctorAv(u, email);
+            if (users != null)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return BadRequest(users);
+            }
+        }
+
+        [HttpGet("getdocbyStatus/{status}")]
+        public ActionResult getdocbystat([FromRoute] bool status)
+        {
+            var users = _logic.getDocByStatus(status);
+            if (users != null)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return BadRequest(users);
+            }
+        }
+    }
+}
---
+++ Doctor_service/Controllers/DoctorController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Doctor_Business_Logic;
+using Model;
+using Doctor;
+
+namespace service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly ILogic _logic;
+        public DoctorController(ILogic logic)
+        {
+            _logic = logic;
+        }
+        [HttpGet("getAll")]
+        public ActionResult Get()
+        {
+            var Doct = _logic.GetAllDocts();
+            if (Doct != null)
+            {
+                return Ok(Doct);
+            }
+            else
+            {
+                return BadRequest(Doct);
+            }
+        }
+        [HttpGet("particular/{email}")]
+        public ActionResult Getp([FromRoute] string email)
+        {
+            var users = _logic.GetDoct(email);
+            if (users != null)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return BadRequest(users);
+            }
+        }
+        [HttpPost("Add")]
+        public ActionResult Post([FromBody] doctorr user)
+        {
+            var users = _logic.ADD(user);
+            if (users != null)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return BadRequest(users);
+            }
+        }
+
+
+
+    }
+}
